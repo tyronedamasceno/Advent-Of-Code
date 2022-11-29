4141c3f668946f716be2e4a548a43f4fb6852234
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-11-29 20:06:58 -0300

    day03 - pt2 isn't good but solves

---
+++ 2021/day03/p1.py
@@ -0,0 +1,23 @@
+with open('inp.txt') as f:
+    l = f.read().split()
+
+d = {
+    i: {"0": set(), "1": set()} for i in range(len(l[0]))
+}
+
+for s in l:
+    for i, b in enumerate(s):
+        d[i][b].add(s)
+
+gamma = ''
+epsilon = ''
+
+for i in range(len(l[0])):
+    if len(d[i]['0']) > len(d[i]['1']):
+        gamma += '0'
+        epsilon += '1'
+    else:
+        gamma += '1'
+        epsilon += '0'
+
+print(int(gamma, 2) * int(epsilon, 2))
---
+++ 2021/day03/p2.py
@@ -0,0 +1,31 @@
+with open('inp.txt') as f:
+    l = f.read().split()
+
+d = {
+    i: {"0": set(), "1": set()} for i in range(len(l[0]))
+}
+
+for s in l:
+    for i, b in enumerate(s):
+        d[i][b].add(s)
+
+to_oxygen = set(l)
+to_carbon = set(l)
+
+i_ox = 0
+while len(to_oxygen) > 1:
+    if len(d[i_ox]['1'] & to_oxygen) >= len(d[i_ox]['0'] & to_oxygen):
+        to_oxygen -= d[i_ox]['0']
+    else:
+        to_oxygen -= d[i_ox]['1']
+    i_ox += 1
+
+i_carb = 0
+while len(to_carbon) > 1:
+    if len(d[i_carb]['0'] & to_carbon) <= len(d[i_carb]['1'] & to_carbon):
+        to_carbon -= d[i_carb]['1']
+    else:
+        to_carbon -= d[i_carb]['0']
+    i_carb += 1
+
+print(int(to_oxygen.pop(), 2) * int(to_carbon.pop(), 2))
