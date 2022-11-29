213dab46b67025aa8de7906df6918c7d9d26cdfb
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-11-29 18:41:27 -0300

    AoC 2021 Day01

---
+++ 2021/day01/p2.py
@@ -0,0 +1,18 @@
+with open('inp.txt') as f:
+    l = [
+        int(n) for n in f.read().split('\n')
+    ]
+
+x = 0
+
+l2 = [
+    sum(l[i:i+3]) for i in range(len(l)-2)
+]
+
+x = 0
+
+for i in range(1, len(l2)):
+    if l2[i] > l2[i-1]:
+        x += 1
+
+print(x)
