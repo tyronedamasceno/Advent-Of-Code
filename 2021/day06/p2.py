d03c2a31b2a3d10daf1c7e45ff326ff602b01d76
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-11-30 18:16:57 -0300

    works for p1

---
+++ 2021/day06/p2.py
@@ -0,0 +1,18 @@
+with open('inp.txt') as f:
+    fishes = [int(x) for x in f.read().split(',')]
+
+days = 1
+new = 0
+
+while days:
+    new = 0
+    for i in range(len(fishes)):
+        if fishes[i] == 0:
+            fishes[i] = 6
+            new += 1
+        else:
+            fishes[i] -= 1
+    fishes += ([8] * new)
+    days -= 1
+
+print(len(fishes))
