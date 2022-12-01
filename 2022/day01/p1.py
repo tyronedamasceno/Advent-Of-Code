1630e10f7d4d909c84e597faf394609aa0b1176d
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-12-01 05:11:10 -0300

    begin 2022 AoC

---
+++ 2022/day01/p1.py
@@ -0,0 +1,9 @@
+with open('inp.txt') as f:
+    cals_per_elf = f.read().split('\n\n')
+
+ans = float('-inf')
+
+for elf in cals_per_elf:
+    ans = max(ans, sum(int(x) for x in elf.split('\n')))
+
+print(ans)
---
+++ 2022/day01/p2.py
@@ -0,0 +1,11 @@
+with open('inp.txt') as f:
+    cals_per_elf = f.read().split('\n\n')
+
+cals_per_elf = [
+    sum(int(x) for x in elf.split('\n'))
+    for elf in cals_per_elf
+]
+
+ans = sum(sorted(cals_per_elf, reverse=True)[:3])
+
+print(ans)
