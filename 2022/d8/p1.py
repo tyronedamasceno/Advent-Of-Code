eb0f18ad699c18df0f62b3140802be071c470c3e
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-12-08 07:17:38 -0300

    d8 izi

--- 2022/d8/p1.py
+++ 2022/d8/p1.py
@@ -1,2 +1,33 @@
 with open('inp.txt') as f:
-    ...
+    trees = [[int(x) for x in list(t)] for t in f.read().splitlines()]
+
+ans = set()
+for i, row in enumerate(trees):
+    biggest = -1
+    for j in range(len(row)):
+        if row[j] > biggest:
+            ans.add((i, j))
+            biggest = row[j]
+
+    biggest = -1
+    for j in range(len(row)-1, -1, -1):
+        if row[j] > biggest:
+            ans.add((i, j))
+            biggest = row[j]
+
+for j in range(len(trees[0])):
+    biggest = -1
+    for i in range(len(trees[j])):
+        # print(i,j)
+        if trees[i][j] > biggest:
+            ans.add((i, j))
+            biggest = trees[i][j]
+
+    biggest = -1
+    for i in range(len(trees[j])-1,-1,-1):
+        if trees[i][j] > biggest:
+            ans.add((i, j))
+            biggest = trees[i][j]
+
+
+print(len(ans))
--- 2022/d8/p2.py
+++ 2022/d8/p2.py
@@ -1,2 +1,48 @@
 with open('inp.txt') as f:
-    ...
+    trees = [[int(x) for x in list(t)] for t in f.read().splitlines()]
+
+ans = -1
+
+def _find_score(i, j, trees):
+    score = 1
+    cur = trees[i][j]
+
+    # down
+    local_score = 0
+    for x in range(i+1, len(trees)):
+        local_score += 1
+        if trees[x][j] >= cur:
+            break
+    score *= local_score
+
+    # right
+    local_score = 0
+    for y in range(j+1, len(trees[0])):
+        local_score += 1
+        if trees[i][y] >= cur:
+            break
+    score *= local_score
+
+    # up
+    local_score = 0
+    for x in range(i-1, -1, -1):
+        local_score += 1
+        if trees[x][j] >= cur:
+            break
+    score *= local_score
+
+    # left
+    local_score = 0
+    for y in range(j-1, -1, -1):
+        local_score += 1
+        if trees[i][y] >= cur:
+            break
+    score *= local_score
+
+    return score
+
+for i in range(1, len(trees)-1):
+    for j in range(1, len(trees[0])-1):
+        ans = max(ans, _find_score(i, j, trees))
+
+print(ans)
