d151df3b26ef6cb22d5d2d8eb3dbf81f5726e8ee
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-12-10 06:47:21 -0300

    d10 p1

--- 2022/d10/p1.py
+++ 2022/d10/p1.py
@@ -1,2 +1,25 @@
 with open('inp.txt') as f:
-    ...
+    lines = f.read().splitlines()
+
+x = 1
+cycle = 0
+so = {20, 60, 100, 140, 180, 220}
+
+ans = 0
+
+for i, line in enumerate(lines):
+    if line == 'noop':
+        cycle += 1
+        if cycle in so:
+            ans += (cycle * x)
+    else:
+        cycle += 1
+        if cycle in so:
+            ans += (cycle * x)
+        cycle += 1
+        _, k = line.split()
+        if cycle in so:
+            ans += (cycle * x)
+        x += int(k)
+
+print(ans)
