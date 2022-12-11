b1b17f34d02607a0a3af01d821c1656660af772c
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-12-11 13:36:55 -0300

    d10 p2 - f*cking vertical alignment

--- 2022/d10/p2.py
+++ 2022/d10/p2.py
@@ -1,2 +1,28 @@
 with open('inp.txt') as f:
-    ...
+    lines = f.read().splitlines()
+
+reg_x = 1
+cycle = -1
+ans = []
+
+def _check_sprite(c, r):
+    if c % 40 in range(r-1, r+2):
+        return '#'
+    return ' '
+
+for line in lines:
+    if line == 'noop':
+        cycle += 1
+        ans.append(_check_sprite(cycle, reg_x))
+    else:
+        cycle += 1
+        ans.append(_check_sprite(cycle, reg_x))
+
+        cycle += 1
+        ans.append(_check_sprite(cycle, reg_x))
+
+        k = int(line.split()[1])
+        reg_x += k
+
+for i in range(0, 240, 40):
+    print(''.join(ans[i:i+40]))
