923381c8d8fbf17f3be271b4dd53e80b3a7e4801
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2022-12-13 07:28:52 -0300

    d13 p1 complicated, p2 izi

--- 2022/d13/p1.py
+++ 2022/d13/p1.py
@@ -1,2 +1,46 @@
+import json
+
+def check_order(left, right):
+    if isinstance(left, int) and isinstance(right, int):
+        if left < right:
+            return 1
+        if left == right:
+            return 0
+        return -1
+
+    if isinstance(left, list) and isinstance(right, int):
+        right = [right]
+
+    if isinstance(left, int) and isinstance(right, list):
+        left = [left]
+
+    for x, y in zip(left, right):
+        tmp = check_order(x, y)
+        if tmp != 0:
+            return tmp
+
+    if len(left) < len(right):
+        return 1
+    elif len(left) == len(right):
+        return 0
+
+
 with open('inp.txt') as f:
-    ...
+    lines = [json.loads(x) for x in f.read().replace('\n\n', '\n').splitlines()]
+
+k = 0
+ans = 0
+
+for i in range(0, len(lines), 2):
+    k += 1
+    a = lines[i]
+    b = lines[i+1]
+
+    # if k == 4:
+    #     breakpoint()
+    if check_order(a, b) == 1:
+        # print(k)
+        ans += k
+
+print(ans)
+    
--- 2022/d13/p2.py
+++ 2022/d13/p2.py
@@ -1,2 +1,38 @@
+from functools import cmp_to_key
+import json
+
+def check_order(left, right):
+    if isinstance(left, int) and isinstance(right, int):
+        if left < right:
+            return 1
+        if left == right:
+            return 0
+        return -1
+
+    if isinstance(left, list) and isinstance(right, int):
+        right = [right]
+
+    if isinstance(left, int) and isinstance(right, list):
+        left = [left]
+
+    for x, y in zip(left, right):
+        tmp = check_order(x, y)
+        if tmp != 0:
+            return tmp
+
+    if len(left) < len(right):
+        return 1
+    elif len(left) == len(right):
+        return 0
+    return -1
+
+
 with open('inp.txt') as f:
-    ...
+    lines = [json.loads(x) for x in f.read().replace('\n\n', '\n').splitlines()]
+
+lines.append([[2]])
+lines.append([[6]])
+
+lines = sorted(lines, key=cmp_to_key(check_order), reverse=True)
+
+print((lines.index([[2]])+1) * (lines.index([[6]])+1))
