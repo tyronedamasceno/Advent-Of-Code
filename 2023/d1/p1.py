f9a0dc8e5ff5dc66f13150288200737cae253dd3
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-01 07:22:54 +0100

    2023 d1

--- 2023/d1/p1.py
+++ 2023/d1/p1.py
@@ -1,2 +1,20 @@
+from string import digits
+
 with open("inp.txt") as f:
-    ...
+    lines = f.read().split("\n")
+
+ans = 0
+
+for line in lines:
+    tmp = ""
+    for i in line:
+        if i in digits:
+            tmp += i
+            break
+    for i in line[::-1]:
+        if i in digits:
+            tmp += i
+            break
+    ans += int(tmp)
+
+print(ans)
--- 2023/d1/p2.py
+++ 2023/d1/p2.py
@@ -1,2 +1,39 @@
+from string import digits
+
 with open("inp.txt") as f:
-    ...
+    lines = f.read().split("\n")
+
+ans = 0
+spelled = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+for line in lines:
+    numbers = []
+    for d in digits:
+        idx = line.find(d)
+        if idx != -1:
+            numbers.append((idx, d))
+        idx = line.rfind(d)
+        if idx != -1:
+            numbers.append((idx, d))
+    for d, v in spelled.items():
+        idx = line.find(d)
+        if idx != -1:
+            numbers.append((idx, v))
+        idx = line.rfind(d)
+        if idx != -1:
+            numbers.append((idx, v))
+
+    numbers.sort()
+    ans += int(f"{numbers[0][1]}{numbers[-1][1]}")
+
+print(ans)
