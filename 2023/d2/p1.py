1d46de70e680e4fcc9b9b1d537dd049c073fc921
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-02 06:23:36 +0100

    2023 d2

--- 2023/d2/p1.py
+++ 2023/d2/p1.py
@@ -1,2 +1,24 @@
 with open("inp.txt") as f:
-    ...
+    lines = f.read().split('\n')
+
+ans = 0
+bads = set()
+
+for line in lines:
+    gn, rounds = line.split(':')
+    gid = int(gn[5:])
+
+    ans += gid
+
+    rounds = rounds.split(';')
+    for round in rounds:
+        round = [r.strip().split() for r in round.split(',')]
+        for k, color in round:
+            if color == 'red' and int(k) > 12:
+                bads.add(gid)
+            if color == 'green' and int(k) > 13:
+                bads.add(gid)
+            if color == 'blue' and int(k) > 14:
+                bads.add(gid)
+
+print(ans - sum(bads))
--- 2023/d2/p2.py
+++ 2023/d2/p2.py
@@ -1,2 +1,25 @@
+from collections import defaultdict
+
 with open("inp.txt") as f:
-    ...
+    lines = f.read().split('\n')
+
+ans = 0
+bads = set()
+
+for line in lines:
+    gn, rounds = line.split(':')
+    gid = int(gn[5:])
+
+    rounds = rounds.split(';')
+
+    d = defaultdict(list)
+
+    for round in rounds:
+        round = [r.strip().split() for r in round.split(',')]
+        for k, color in round:
+            k = int(k)
+            d[color].append(k)
+    power = max(d['red']) * max(d['green']) * max(d['blue'])
+    ans += power
+
+print(ans)
