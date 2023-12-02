fc0609193fc5ad61d7504230561c15ee9e17e891
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-02 07:17:09 +0100

    Improve 2023/d2/p2 code

--- 2023/d2/p2.py
+++ 2023/d2/p2.py
@@ -4,22 +4,19 @@ with open("inp.txt") as f:
     lines = f.read().split('\n')
 
 ans = 0
-bads = set()
-
 for line in lines:
     gn, rounds = line.split(':')
     gid = int(gn[5:])
 
     rounds = rounds.split(';')
 
-    d = defaultdict(list)
+    d = defaultdict(int)
 
     for round in rounds:
         round = [r.strip().split() for r in round.split(',')]
         for k, color in round:
-            k = int(k)
-            d[color].append(k)
-    power = max(d['red']) * max(d['green']) * max(d['blue'])
+            d[color] = max(d[color], int(k))
+    power = d['red'] * d['green'] * d['blue']
     ans += power
 
 print(ans)
