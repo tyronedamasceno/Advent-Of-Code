eb0da0f94837af303765491fd04e98bfe1aa8a7a
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-03 06:37:08 +0100

    2023 d3

--- 2023/d3/p1.py
+++ 2023/d3/p1.py
@@ -1,2 +1,49 @@
-with open("inp.txt") as f:
+from collections import defaultdict
+from string import digits
+
+with open("inp2.txt") as f:
     lines = f.read().split('\n')
+
+ans = 0
+sd = defaultdict(bool)
+symbols = {'#', '$', '%', '&', '*', '+', '-', '/', '=', '@'}
+
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c in symbols:
+            sd[(i, j)] = True
+
+seen = set()
+k = ''
+pos = []
+
+
+def _check_adjacencies():
+    adjs = [
+        (-1, -1), (0, -1), (1, -1),
+        (-1, 0), (1, 0),
+        (-1, 1), (0, 1), (1, 1)
+    ]
+    for x, y in pos:
+        for a, b in adjs:
+            if (x + a, y + b) in sd:
+                return int(k)
+
+    return 0
+
+
+for i, line in enumerate(lines):
+    if k:
+        ans += _check_adjacencies()
+        k = ''
+        pos = []
+    for j, c in enumerate(line):
+        if c in symbols or c == '.':
+            ans += _check_adjacencies()
+            k = ''
+            pos = []
+        if c in digits:
+            k += c
+            pos.append((i, j))
+
+print(ans)
--- 2023/d3/p2.py
+++ 2023/d3/p2.py
@@ -1,2 +1,58 @@
-with open("inp.txt") as f:
+from collections import defaultdict
+from string import digits
+
+with open("inp2.txt") as f:
     lines = f.read().split('\n')
+
+ans = 0
+sd = defaultdict(bool)
+gears = []
+numbers = []
+
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c == '*':
+            gears.append((i, j))
+
+seen = set()
+k = ''
+pos = []
+
+
+for i, line in enumerate(lines):
+    if k:
+        numbers.append((k, list(pos)))
+        k = ''
+        pos = []
+    for j, c in enumerate(line):
+        if k and c not in digits:
+            numbers.append((k, list(pos)))
+            k = ''
+            pos = []
+        if c in digits:
+            k += c
+            pos.append((i, j))
+
+
+def _check_adjacencies(xg, yg):
+    tmp = []
+    adjs = [
+        (-1, -1), (0, -1), (1, -1),
+        (-1, 0), (1, 0),
+        (-1, 1), (0, 1), (1, 1)
+    ]
+    for number, positions in numbers:
+        for a, b in adjs:
+            if (xg + a, yg + b) in positions:
+                tmp.append(int(number))
+                break
+    return tmp
+
+
+for gear in gears:
+    x = _check_adjacencies(*gear)
+    if len(x) == 2:
+        ans += (x[0] * x[1])
+
+
+print(ans)
