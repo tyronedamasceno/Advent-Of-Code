d9d93382b66c09021e5c7af5c21010178c056cc6
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-04 07:55:35 +0100

    2023 d4

--- 2023/d4/p1.py
+++ 2023/d4/p1.py
@@ -1,2 +1,15 @@
-with open("inp.txt") as f:
+with open("inp2.txt") as f:
     lines = f.read().split('\n')
+
+ans = 0
+
+for line in lines:
+    _, numbers = line.split(':')
+    winner, ours = numbers.split('|')
+    ws = set(winner.split())
+    os = set(ours.split())
+    matches = ws & os
+    if matches:
+        ans += 2 ** (len(matches) - 1)
+
+print(ans)
--- 2023/d4/p2.py
+++ 2023/d4/p2.py
@@ -1,2 +1,27 @@
-with open("inp.txt") as f:
+from queue import Queue
+
+with open("inp2.txt") as f:
     lines = f.read().split('\n')
+
+ans = 0
+d = {}
+q = Queue()
+
+for line in lines:
+    cid, numbers = line.split(':')
+    cid = int(cid[4:])
+    winner, ours = numbers.split('|')
+    ws = set(winner.split())
+    os = set(ours.split())
+    matches = ws & os
+    d[cid] = len(matches)
+    q.put(cid)
+
+
+while not q.empty():
+    ans += 1
+    k = q.get()
+    for i in range(k + 1, k + d[k] + 1):
+        q.put(i)
+
+print(ans)
