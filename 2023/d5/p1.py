1e1c8c4767121a2e24bca74bcefa2bbb0995874b
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-05 19:54:40 +0100

    p1 worked =D

--- 2023/d5/p1.py
+++ 2023/d5/p1.py
@@ -13,18 +13,26 @@ for line in lines[1:]:
         continue
 
     dst_base, src_base, lenght = [int(k) for k in line.split()]
-    for i in range(lenght):
-        d[src][src_base + i] = dst_base + i
+    d[src][src_base] = (dst_base, lenght)
+
+
+def _get_mapping(value, resource):
+    mappings = sorted(d[resource].items())
+    for src, (dst, l) in mappings:
+        if value >= src and value < src + l:
+            return dst + value - src
+    return value
+
 
 ans = float('inf')
 for seed in seeds:
-    soil = d['seed'].get(seed, seed)
-    ferti = d['soil'].get(soil, soil)
-    water = d['fertilizer'].get(ferti, ferti)
-    light = d['water'].get(water, water)
-    temp = d['light'].get(light, light)
-    humi = d['temperature'].get(temp, temp)
-    loc = d['humidity'].get(humi, humi)
-    ans = min(ans, loc)
+    soil = _get_mapping(seed, 'seed')
+    fertilizer = _get_mapping(soil, 'soil')
+    water = _get_mapping(fertilizer, 'fertilizer')
+    light = _get_mapping(water, 'water')
+    temperature = _get_mapping(light, 'light')
+    humidity = _get_mapping(temperature, 'temperature')
+    location = _get_mapping(humidity, 'humidity')
+    ans = min(ans, location)
 
 print(ans)
