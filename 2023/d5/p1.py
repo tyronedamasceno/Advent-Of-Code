6903fe42a354d5e5c3b8074a18a7b90e9577062d
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-05 11:15:15 +0100

    2023 d5 p1 - not working

--- 2023/d5/p1.py
+++ 2023/d5/p1.py
@@ -1,2 +1,30 @@
+from collections import defaultdict
+
 with open("inp.txt") as f:
     lines = f.read().split('\n')
+    lines = [line for line in lines if line]
+
+d = defaultdict(dict)
+
+seeds = [int(k) for k in lines[0][7:].split()]
+for line in lines[1:]:
+    if 'map' in line:
+        src, _, dst = line[:-5].split('-')
+        continue
+
+    dst_base, src_base, lenght = [int(k) for k in line.split()]
+    for i in range(lenght):
+        d[src][src_base + i] = dst_base + i
+
+ans = float('inf')
+for seed in seeds:
+    soil = d['seed'].get(seed, seed)
+    ferti = d['soil'].get(soil, soil)
+    water = d['fertilizer'].get(ferti, ferti)
+    light = d['water'].get(water, water)
+    temp = d['light'].get(light, light)
+    humi = d['temperature'].get(temp, temp)
+    loc = d['humidity'].get(humi, humi)
+    ans = min(ans, loc)
+
+print(ans)
