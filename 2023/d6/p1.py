9c88e7aba8c31ade04f77ac132e5aa7112ee512a
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-06 11:43:06 +0100

    d6 p1

--- 2023/d6/p1.py
+++ 2023/d6/p1.py
@@ -1,2 +1,20 @@
 with open("inp.txt") as f:
     lines = f.read().split('\n')
+
+times = [int(x) for x in lines[0].split(':')[-1].split()]
+dists = [int(x) for x in lines[1].split(':')[-1].split()]
+
+ans = 1
+
+for time, dist in zip(times, dists):
+    k = 0
+
+    for i in range(time + 1):
+        v = i
+        mdist = v * (time - i)
+        if mdist > dist:
+            k += 1
+
+    ans *= k
+
+print(ans)
