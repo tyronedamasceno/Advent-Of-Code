c450bd020b1a945300aa4b49d6461bdc94e6b143
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-07 08:10:52 +0100

    2023 d7 p1 and p2

--- 2023/d7/p2.py
+++ 2023/d7/p2.py
@@ -1,2 +1,78 @@
+from collections import Counter
+import functools
+
+
 with open("inp.txt") as f:
     lines = f.read().split('\n')
+
+
+def hand_level(hand):
+    if 'J' in hand:
+        hand_new = ''
+        js = 0
+        for c in hand:
+            if c == 'J':
+                js += 1
+            else:
+                hand_new += c
+        kinds = sorted(Counter(hand_new).values(), reverse=True)
+        if kinds == []:
+            kinds = [5]
+        else:
+            kinds[0] += js
+    else:
+        kinds = sorted(Counter(hand).values(), reverse=True)
+    if kinds == [5]:
+        return 1
+    if kinds == [4, 1]:
+        return 2
+    if kinds == [3, 2]:
+        return 3
+    if kinds == [3, 1, 1]:
+        return 4
+    if kinds == [2, 2, 1]:
+        return 5
+    if kinds == [2, 1, 1, 1]:
+        return 6
+    return 7
+
+
+def compare_rank(c1, c2):
+    seq = 'AKQT98765432J'
+    i1 = seq.index(c1)
+    i2 = seq.index(c2)
+    if i1 < i2:
+        return -1
+    if i1 == i2:
+        return 0
+    return 1
+
+
+def compare(h1, h2):
+    l1 = hand_level(h1[0])
+    l2 = hand_level(h2[0])
+
+    if l1 < l2:
+        return -1
+    if l1 > l2:
+        return 1
+
+    for c1, c2 in zip(h1[0], h2[0]):
+        cmp_k = compare_rank(c1, c2)
+        if not cmp_k:
+            continue
+        return cmp_k
+    return -1
+
+
+hands_bids = []
+for line in lines:
+    h, b = line.split()
+    hands_bids.append((h, int(b)))
+
+sorted_hands = sorted(hands_bids, key=functools.cmp_to_key(compare), reverse=True)
+
+ans = 0
+for idx, (_, bid) in enumerate(sorted_hands):
+    ans += (bid * (idx + 1))
+print(ans)
