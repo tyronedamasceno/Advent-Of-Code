a0ab838fc0d154473634a982c5a89947c1651613
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-09 07:36:46 +0100

    d9 p1

--- 2023/d9/p1.py
+++ 2023/d9/p1.py
@@ -1,2 +1,22 @@
 with open("inp.txt") as f:
     lines = f.read().split('\n')
+
+ans = 0
+
+for line in lines:
+    cur = [int(x) for x in line.split()]
+    tmp = [list(cur)]
+    while set(tmp[-1]) != {0}:
+        cur = [
+            cur[i] - cur[i - 1]
+            for i in range(1, len(cur))
+        ]
+        tmp.append(list(cur))
+
+    tmp[-1].append(0)
+    for i in range(len(tmp) - 2, -1, -1):
+        tmp[i].append(tmp[i + 1][-1] + tmp[i][-1])
+
+    ans += tmp[0][-1]
+
+print(ans)
