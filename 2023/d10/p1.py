c819246a143c845f2376a7af7e2e2b5ccc9f1bba
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-10 10:10:04 +0100

    d10 p1

--- 2023/d10/p1.py
+++ 2023/d10/p1.py
@@ -1,2 +1,46 @@
+from queue import Queue
+
 with open("inp.txt") as f:
     lines = f.read().split('\n')
+
+for i, l in enumerate(lines):
+    if 'S' in l:
+        start = i, l.index('S')
+        break
+
+q = Queue()
+q.put(start)
+
+seen = set()
+
+
+def _get_directions(pipe):
+    match pipe:
+        case '|':
+            return [(1, 0), (-1, 0)]
+        case '-':
+            return [(0, 1), (0, -1)]
+        case 'F':
+            return [(1, 0), (0, 1)]
+        case 'J':
+            return [(-1, 0), (0, -1)]
+        case 'L':
+            return [(-1, 0), (0, 1)]
+        case '7':
+            return [(1, 0), (0, -1)]
+
+
+while not q.empty():
+    i, j = q.get()
+    seen.add((i, j))
+    pipe = lines[i][j]
+    if pipe == 'S':
+        pipe = 'F'
+
+    dirs = _get_directions(pipe)
+    for x, y in dirs:
+        nxt = (i + x, j + y)
+        if nxt not in seen:
+            q.put(nxt)
+
+print(len(seen) / 2)
