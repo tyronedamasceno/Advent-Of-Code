1be8173b874ad9aae324d75d67a7623ccf40c56e
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-14 11:55:09 +0100

    d14 p1

--- 2023/d14/p1.py
+++ 2023/d14/p1.py
@@ -1,2 +1,18 @@
 with open("inp.txt") as f:
     lines = f.read().split('\n')
+
+height = len(lines)
+
+ans = 0
+
+for col in range(len(lines[0])):
+    stop = 0
+    for idx, line in enumerate(lines):
+        cur = line[col]
+        if cur == '#':
+            stop = idx + 1
+        if cur == 'O':
+            ans += (height - stop)
+            stop += 1
+
+print(ans)
