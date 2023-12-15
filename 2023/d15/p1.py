027568c70bd78389ec9ca0a0a5cc0f573f0a2a46
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2023-12-15 10:11:02 +0100

    d15 p1

--- 2023/d15/p1.py
+++ 2023/d15/p1.py
@@ -1,2 +1,14 @@
 with open("inp.txt") as f:
     lines = f.read().split('\n')
+
+
+ans = 0
+for seq in lines[0].split(','):
+    tmp = 0
+    for c in seq:
+        tmp += ord(c)
+        tmp *= 17
+        tmp %= 256
+    ans += tmp
+
+print(ans)
