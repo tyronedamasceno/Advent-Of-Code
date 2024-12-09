c4353927c12b0b6a969056ad6160a9bcfb9d37a3
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2024-12-09 12:14:03 +0100

    optimize performance for part 2, ~3s now

--- 2024/d9/p2.py
+++ 2024/d9/p2.py
@@ -3,24 +3,31 @@ with open('inp.txt') as f:
 
 diskmap = []
 idx = 0
-ranges = {}
+ranges_f = {}
+ranges_b = []
 for i, v in enumerate(line):
     if i % 2 == 0:
         a = len(diskmap)
         diskmap.extend(int(v) * [idx])
         b = len(diskmap)
-        ranges[idx] = (a, b)
+        ranges_f[idx] = (a, b)
         idx += 1
     else:
+        a = len(diskmap)
         diskmap.extend(int(v) * '.')
+        b = len(diskmap)
+        ranges_b.append((a, b))
 
 file = idx - 1
 
 while file >= 0:
-    a, b = ranges[file]
-    for i in range(a):
-        if diskmap[i:i + (b - a)] == ['.'] * (b - a):
-            diskmap[i:i + (b - a)], diskmap[a:b] = diskmap[a:b], diskmap[i:i + (b - a)]
+    a, b = ranges_f[file]
+    for i, (x, y) in enumerate(ranges_b):
+        if y > a:
+            break
+        if y - x >= b - a:
+            diskmap[x:x + (b - a)], diskmap[a:b] = diskmap[a:b], diskmap[x:x + (b - a)]
+            ranges_b[i] = (x + b - a, y)
             break
     file -= 1
 
