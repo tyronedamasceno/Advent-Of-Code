c49244d14b3af4ce70d1c3779b235b9a9e764cc0
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2024-12-10 10:27:44 +0100

    2015/d15 super brute force

--- 2015/d15/p1.py
+++ 2015/d15/p1.py
@@ -1,24 +1,37 @@
-from itertools import permutations
-
 with open('inp.txt') as f:
     lines = f.read().splitlines()
 
-dists = {}
-cities = set()
+ings = []
+cap = {}
+dur = {}
+fla = {}
+tex = {}
+# cal = {}
 
 for line in lines:
-    c1, _, c2, _, d = line.split()
-    cities.update({c1, c2})
-    dists[(c1, c2)] = int(d)
-    dists[(c2, c1)] = int(d)
+    ing, props = line.split(': ')
+    ings.append(ing)
+    props = props.split(', ')
+    props = [int(p.split()[1]) for p in props]
+    cap[ing] = props[0]
+    dur[ing] = props[1]
+    fla[ing] = props[2]
+    tex[ing] = props[3]
+    # cal[ing] = props[4]
 
-ans = 9999999999999
+ans = -1
+for i in range(1, 101):
+    for j in range(1, 101):
+        for k in range(1, 101):
+            for h in range(1, 101):
+                if i + j + k + h != 100:
+                    continue
+                caps = max(0, i * cap[ings[0]] + j * cap[ings[1]] + k * cap[ings[2]] + h * cap[ings[3]])
+                durs = max(0, i * dur[ings[0]] + j * dur[ings[1]] + k * dur[ings[2]] + h * dur[ings[3]])
+                flas = max(0, i * fla[ings[0]] + j * fla[ings[1]] + k * fla[ings[2]] + h * fla[ings[3]])
+                texs = max(0, i * tex[ings[0]] + j * tex[ings[1]] + k * tex[ings[2]] + h * tex[ings[3]])
 
-for p in permutations(cities):
-    tmp = 0
-    for i in range(1, len(cities)):
-        tmp += dists[(p[i - 1], p[i])]
+                ans = max(ans, caps * durs * flas * texs)
 
-    ans = min(ans, tmp)
 
 print(ans)
