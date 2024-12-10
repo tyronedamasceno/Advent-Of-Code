d066740ccd504233b9b8c290d0f9f2ac0dfc207a
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2024-12-10 10:31:40 +0100

    less brute-force

--- 2015/d15/p1.py
+++ 2015/d15/p1.py
@@ -21,17 +21,16 @@ for line in lines:
 
 ans = -1
 for i in range(1, 101):
-    for j in range(1, 101):
-        for k in range(1, 101):
-            for h in range(1, 101):
-                if i + j + k + h != 100:
-                    continue
-                caps = max(0, i * cap[ings[0]] + j * cap[ings[1]] + k * cap[ings[2]] + h * cap[ings[3]])
-                durs = max(0, i * dur[ings[0]] + j * dur[ings[1]] + k * dur[ings[2]] + h * dur[ings[3]])
-                flas = max(0, i * fla[ings[0]] + j * fla[ings[1]] + k * fla[ings[2]] + h * fla[ings[3]])
-                texs = max(0, i * tex[ings[0]] + j * tex[ings[1]] + k * tex[ings[2]] + h * tex[ings[3]])
+    for j in range(1, 101 - i):
+        for k in range(1, 101 - i - j):
+            h = 100 - i - j - k
 
-                ans = max(ans, caps * durs * flas * texs)
+            caps = max(0, i * cap[ings[0]] + j * cap[ings[1]] + k * cap[ings[2]] + h * cap[ings[3]])
+            durs = max(0, i * dur[ings[0]] + j * dur[ings[1]] + k * dur[ings[2]] + h * dur[ings[3]])
+            flas = max(0, i * fla[ings[0]] + j * fla[ings[1]] + k * fla[ings[2]] + h * fla[ings[3]])
+            texs = max(0, i * tex[ings[0]] + j * tex[ings[1]] + k * tex[ings[2]] + h * tex[ings[3]])
+
+            ans = max(ans, caps * durs * flas * texs)
 
 
 print(ans)
