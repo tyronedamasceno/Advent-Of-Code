83f155172c04e257b21307ef7ddfda46d7d5fd31
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2024-12-11 14:30:42 +0100

    remove unnecessary step

--- 2024/d11/p2.py
+++ 2024/d11/p2.py
@@ -9,21 +9,17 @@ for s in map(int, line.split()):
 
 
 for _ in range(75):
-    ks = list(stones.keys())
     new = defaultdict(int)
-    for k in ks:
+    for k in stones.keys():
         if k == 0:
             new[1] += stones[k]
-            stones.pop(k)
         elif len(str(k)) % 2 == 0:
             sst = str(k)
             a, b = sst[:len(sst) // 2], sst[len(sst) // 2:]
             new[int(a)] += stones[k]
             new[int(b)] += stones[k]
-            stones.pop(k)
         else:
             new[k * 2024] += stones[k]
-            stones.pop(k)
 
     stones = new
 
