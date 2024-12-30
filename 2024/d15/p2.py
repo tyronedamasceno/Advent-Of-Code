4a71abfeb5e7268c9e0af82bdc0a5668fa947d94
Tyrone Damasceno <tyrone.damasceno@fareharbor.com>
2024-12-30 14:59:40 +0100

    2024/d15 p2 - no recursive

--- 2024/d15/p2.py
+++ 2024/d15/p2.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+from tqdm import tqdm
+
 with open('inp.txt') as f:
     tmp_grid, rules = f.read().split('\n\n')
 
@@ -27,7 +30,6 @@ def print_grid():
     for l in grid:
         print(''.join(l))
 
-print_grid()
 
 rmap = {
     '<': (0, -1),
@@ -36,44 +38,50 @@ rmap = {
     'v': (1, 0),
 }
 
-
-def move(rx, ry, dx, dy):
-    if grid[rx + dx][ry + dy] == '#':
-        return False
-
-    if grid[rx + dx][ry + dy] == '[':
-        if move(rx + dx, ry + dy, dx, dy) and move(rx + dx, ry + dy + 1, dx, dy):
-            grid[rx][ry], grid[rx + dx][ry + dy] = grid[rx + dx][ry + dy], grid[rx][ry]
-            grid[rx][ry], grid[rx + dx][ry + dy + 1] = grid[rx + dx][ry + dy + 1], grid[rx][ry]
-            return True
-
-    if grid[rx + dx][ry + dy] == ']':
-        if move(rx + dx, ry + dy, dx, dy) and move(rx + dx, ry + dy - 1, dx, dy):
-            grid[rx][ry], grid[rx + dx][ry + dy] = grid[rx + dx][ry + dy], grid[rx][ry]
-            grid[rx][ry], grid[rx + dx][ry + dy - 1] = grid[rx + dx][ry + dy - 1], grid[rx][ry]
-            return True
-
-    if grid[rx + dx][ry + dy] == '.':
-        grid[rx][ry], grid[rx + dx][ry + dy] = grid[rx + dx][ry + dy], grid[rx][ry]
-        return True
-
-    return False
-
-
-for rule in rules:
+for rule in tqdm(rules):
     if rule == '\n':
         continue
     dx, dy = rmap[rule]
 
-    if move(rx, ry, dx, dy):
-        rx, ry = rx + dx, ry + dy
+    to_move = [(rx, ry)]
+    i = 0
+    flag = False
 
+    while i < len(to_move):
+        x, y = to_move[i]
+        nx, ny = x + dx, y + dy
+        if grid[nx][ny] == '[':
+            if (nx, ny) not in to_move:
+                to_move.append((nx, ny))
+            if (nx, ny + 1) not in to_move:
+                to_move.append((nx, ny + 1))
+        elif grid[nx][ny] == ']':
+            if (nx, ny) not in to_move:
+                to_move.append((nx, ny))
+            if (nx, ny - 1) not in to_move:
+                to_move.append((nx, ny - 1))
+        elif grid[nx][ny] == '#':
+            flag = True
+            break
+
+        i += 1
+
+    if flag:
+        continue
+    tmp = deepcopy(grid)
+    for x, y in to_move:
+        tmp[x][y] = '.'
+    for x, y in to_move:
+        tmp[x + dx][y + dy] = grid[x][y]
+
+    grid = tmp
+    rx += dx
+    ry += dy
 
 ans = 0
+
 for i in range(len(grid)):
     for j in range(len(grid[0])):
         if grid[i][j] == '[':
             ans += (100 * i + j)
-
-print_grid()
 print(ans)
